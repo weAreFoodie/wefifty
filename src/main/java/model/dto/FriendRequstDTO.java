6301475e066ba5f2a98f6989b9cfe1c417a55515
dyoun12 <gdaeyeon002@gmail.com>
2025-02-06 18:51:34 +0900

    feat : FriendRequestDTO 추가

---
+++ src/main/java/model/dto/FriendRequstDTO.java
@@ -0,0 +1,21 @@
+package model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class FriendRequstDTO {
+	private int id;
+	private int senderId;
+	private int receiverId;
+	private char status;
+}
