5f3854dbfbe97b68af90b931e062d89061aa865e
태규 <123963462+EOTAEGYU@users.noreply.github.com>
2025-02-07 12:02:03 +0900

    refactor: 기본 DAO CRUD 메소드  추가

--- src/main/java/model/dao/FriendRequestDAO.java
+++ src/main/java/model/dao/FriendRequestDAO.java
@@ -3,6 +3,7 @@ package model.dao;
 import java.util.List;
 
 import model.dto.FriendRequstDTO;
+import model.dto.UserDTO;
 
 public class FriendRequestDAO {
 	// 친구 요청 정보 추가하기
@@ -10,13 +11,18 @@ public class FriendRequestDAO {
 		
 	}
 	
-	// 친구 요청 정보 변경하기
+	// 친구 요청 정보 변경하기(수락, 거절)
 	void updateFriendRequest(int requestId) {
 		
 	}
 	
 	// 해당 회원 친구 요청 목록 가져오기
 	List<FriendRequstDTO> findFriendRequestsByUserId(int userId) {
+		return null;
+	}
+	
+	// 같은 학교, 졸업 년도(gap) 친구 목록 받아오기
+	List<UserDTO> findFriendsBySchoolAndGradYear(String schoolName, int gradYear, int gap){
 		return null;
 	}
 	
