92d424a9c623ae5ea13d456acfc53524f0190970
DanidanaCDY <lucy2315.ce@gmail.com>
2025-02-08 09:37:55 +0900

    feat : FriendInfoListDTO, UserSchoolSummaryDTO 추가

---
+++ src/main/java/model/dto/FriendInfoListDTO.java
@@ -0,0 +1,26 @@
+package model.dto;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class FriendInfoListDTO {
+	private int userId;
+	private String nickname;
+	private String bio;
+	private String name;
+	private char gender;
+	private LocalDate birth;
+	private String profilePicture;
+}
---
+++ src/main/java/model/dto/UserSchoolSummaryDTO.java
@@ -0,0 +1,19 @@
+package model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class UserSchoolSummaryDTO {
+	private String schoolName;
+	private int gradYear;
+}
