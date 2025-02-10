ebd43945befcbf97fbb3d854fe889544dbbaf3fd
min-jp <minjpb@gmail.com>
2025-02-10 11:36:53 +0900

    feat : 세션 기반 id 기능 추가

--- src/main/java/controller/action/GetFriendListAction.java
+++ src/main/java/controller/action/GetFriendListAction.java
@@ -19,9 +19,7 @@ public class GetFriendListAction implements Action {
 		// 세션에서 userId 가져오기
 		HttpSession session = request.getSession();
 
-		// FIXME 세션 구현 전 임시 코드
-		// int userId = (int)session.getAttribute("userId");
-		int userId = 3;
+		int userId = (int)session.getAttribute("userIdKey");
 
 		try {
 			// 유저아이디로 친구 리스트 찾기
